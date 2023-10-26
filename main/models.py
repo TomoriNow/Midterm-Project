1048681f9212d294a2d4d594b370ab1cf299edae
Sirered <gikurniawan.1995@gmail.com>
2023-10-26 22:15:54 +0700

    fixing

--- main/models.py
+++ main/models.py
@@ -20,7 +20,7 @@ class Book(models.Model):
     type = models.CharField(max_length=15,choices=TYPE_CHOICES, default=MANGA)
     author = models.CharField(max_length = 30, null=True, blank= True)
     description = models.TextField(null=True, blank=True)
-    taglist = models.TextField(null=True, blank=True)
+    tags = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.name
