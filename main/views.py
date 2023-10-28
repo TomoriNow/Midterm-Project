aafe21bdb0014c99a41db2807dc5528a86335fea
Sirered <gikurniawan.1995@gmail.com>
2023-10-28 15:15:32 +0700

    adding tags

--- main/views.py
+++ main/views.py
@@ -165,11 +165,13 @@ def show_json_by_id(request, id):
 @login_required(login_url='/login')
 def show_book_entry(request):
     data = Book_Entry.objects.filter(user = request.user)
+    tags = Tag.objects.all()
     context = {"book_entries": data,
                'name':request.user.username,
                'owner': request.user.username,
                'not_owner': "false",
-               'is_owner':True
+               'is_owner':True,
+               'tags': tags
                }
     return render(request, "book_entry.html", context)
 
@@ -208,6 +210,8 @@ def create_custom_entry(request):
     print(form_2.is_valid())
     if form.is_valid() and form_2.is_valid() and request.method == "POST":
         print("success")
+        list = request.POST.getlist('tag')
+        print(list)
         book_entry = form_2.save(commit=False)
         book_entry.user = request.user
         book_entry.last_read_date = datetime.datetime.now()
@@ -215,6 +219,8 @@ def create_custom_entry(request):
         custom_entry.entry = book_entry
         book_entry.save()
         custom_entry.save()
+        custom_entry.taggits.set(list, clear=True)
+        print(custom_entry.taggits)
         book = {
             "id" : book_entry.pk,
             "name": custom_entry.name,
